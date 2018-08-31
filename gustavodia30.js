94a8f6502e8076724025e01bbf5cd40f178387c3
gustavooliver26 <gustavo.ferreira.gavioes@hotmail.com>
2018-08-30 22:20:06 -0300

    incluir os exercicios dia3008

---
+++ gustavodia30.js
@@ -0,0 +1,10 @@
+function multiplica (a,b){
+       console.log("function multiplica (a.b) {...");
+	   return a*b;
+	   console.log("imprimindo resultado" + resultado);
+	   return a*b;
+	   
+}
+     var resultadodafuncaomultiplica = multiplica(10,7);
+     alert( resultadodafuncaomultiplica );
+	 
